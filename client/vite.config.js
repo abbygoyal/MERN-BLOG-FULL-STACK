614f4fa789581127af8153ea5edb57a0a05ec0d6
Abhishek Goyal <110916774+abbygoyal@users.noreply.github.com>
2025-02-14 22:30:48 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,6 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-      "./pages": path.resolve(__dirname, "./src/pages"), // Add this alias
     },
   },
 });
